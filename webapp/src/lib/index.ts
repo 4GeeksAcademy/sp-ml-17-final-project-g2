2036978e9c002eeeac21ed802a54b1fd98759935
Théo Gédin <theo.gedin@gmail.com>
2025-09-04 21:48:48 +0000

    Add missing src/lib/index.ts file for module resolution

---
+++ webapp/src/lib/index.ts
@@ -0,0 +1,8 @@
+// Re-export all functions from database module
+export { 
+  executePythonScript, 
+  getCountries, 
+  getIndicators, 
+  getCountryPerformance, 
+  getCountrySummary 
+} from './database';
